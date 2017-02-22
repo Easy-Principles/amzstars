ad97ca110a69b6dd0ab9d1254181643f3683290c
nadin <nadin@192.168.10.1>
2017-02-22 14:24:52 +0100

    u will see

--- src/amzstars_git/main.java
+++ src/amzstars_git/main.java
@@ -1,10 +1,11 @@
 package amzstars_git;
+import java.util.*;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       System.out.println("Ich liebe dich!");
 	}
 
 }
